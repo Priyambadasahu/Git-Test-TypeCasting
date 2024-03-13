1b7370db7069a0cc609dd1a66ae2bb6febc6207f
Priyambada2015 <priyambadasahu79@gmail.com>
2024-03-13 12:31:06 +0530

    This is first commit to push hte code from eclipse to github

---
+++ src/com/kodnest/Typecasting/Solution1.java
@@ -0,0 +1,21 @@
+package com.kodnest.Typecasting;
+
+public class Solution1 {
+	public static void main(String[]args)
+	{
+		//byte b =10;
+		 //char c ='A';
+		 //byte  = (byte)i;
+		   int  i =65;
+		   char c = (char) i;
+		//long l =b;
+		//float f =b;
+		//double d =b;
+		//char c= (char) 65;
+		System.out.println(i);
+		System.out.println(c);
+		
+		
+	}
+
+}
